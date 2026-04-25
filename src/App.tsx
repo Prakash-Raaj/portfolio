e370c51932682ed81997d11b5bb2840259c3b810
prakash-raaj <prakashraaj7996@gmail.com>
2026-04-25 19:53:19 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-amber-50">portfolio's main page</div>
+    </>
+  )
+}
+
+export default App
